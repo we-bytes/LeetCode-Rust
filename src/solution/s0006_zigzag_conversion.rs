f0ee2621f4a8325cf785e24650ea10ae9a0f7373
webytes <webytes@qq.com>
2024-01-06 22:49:03 +0800

    s0006_zigzag_conversion

---
+++ src/solution/s0006_zigzag_conversion.rs
@@ -0,0 +1,33 @@
+use core::num;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let num_rows = num_rows as usize;
+        let mut arr = vec![String::new(); num_rows];
+        let iter = (0..num_rows).chain((1..num_rows - 1).rev()).cycle();
+        println!("{:?}", iter);
+        iter.zip(s.chars()).for_each(|(i, c)| arr[i].push(c));
+        arr.into_iter().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(
+            Solution::convert("PAYPALISHIRING".to_string(), 4),
+            "PINALSIGYAHRPI"
+        );
+        assert_eq!(
+            Solution::convert("PAYPALISHIRING".to_string(), 3),
+            "PAHNAPLSIIGYIR"
+        );
+        assert_eq!(Solution::convert("A".to_string(), 1), "A");
+        assert_eq!(Solution::convert("AY".to_string(), 2), "AY");
+    }
+}

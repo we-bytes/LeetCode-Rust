7828d84c58dc58cbe85955ad3c69636681341643
webytes <webytes@qq.com>
2024-01-09 18:18:45 +0800

    s0009_palindrome_number

---
+++ src/solution/s0009_palindrome_number.rs
@@ -0,0 +1,23 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        x.to_string() == x.to_string().chars().rev().collect::<String>() // Rust反转字符串
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(Solution::is_palindrome(-32), false);
+        assert_eq!(Solution::is_palindrome(10), false);
+        assert_eq!(Solution::is_palindrome(0), true);
+        assert_eq!(Solution::is_palindrome(9), true);
+        assert_eq!(Solution::is_palindrome(121), true);
+        assert_eq!(Solution::is_palindrome(2222), true);
+        assert_eq!(Solution::is_palindrome(11222211), true);
+    }
+}
